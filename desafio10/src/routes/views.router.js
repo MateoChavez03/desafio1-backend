a2c0e73b84db7094590c4320e206294541b127cd
Mateo Chavez <mateochavez424@gmail.com>
2023-02-16 10:30:56 -0300

    desafío 10

--- desafio10/src/routes/views.router.js
+++ desafio10/src/routes/views.router.js
@@ -3,22 +3,24 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/register',(req,res)=>{
-    //res.redirect('login')
     res.render('register');
 })
 
 router.get('/login', (req,res)=>{
     if (req.session.user) {
-        console.log(req.session.user.name);
         res.redirect('/home')
     } else {
-        console.log("no funciona");
         res.render('login')
     }
 })
 
-router.get('/home', (req,res) => {
-    res.render('home')
+router.get('/home', async (req,res) => {
+    res.render('home', {user: req.session.user})
+})
+
+router.get('/logout', async (req, res) => {
+    res.render('logout', {user: req.session.user});
+    req.session.destroy()
 })
 
 export default router;
