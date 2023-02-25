8e3795d525469d0546e2fc5e17892b44614a6fda
Mateo Chavez <mateochavez424@gmail.com>
2023-02-25 20:51:30 -0300

    Desafío 12 parte 1

---
+++ desafio12/src/config/passport.js
@@ -0,0 +1,49 @@
+import passport from 'passport';
+import local from 'passport-local';
+import { validatePassword } from '../utils.js';
+import GithubStrategy from 'passport-github2';
+import userModel from '../models/User.js';
+import config from './dotenv.js'
+
+const LocalStrategy = local.Strategy;
+
+const initializeStrategies = () => {
+    passport.use('login', new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        if (!email || !password) return done(null, false, { message: "Valores incompletos" })
+        const user = await userModel.findOne({ email })
+        if (!user) return done(null, false, { message: "Credenciales inválidas" })
+        const isValidPassword = await validatePassword(password, user.password);
+        if (!isValidPassword) return done(null, false, { message: "Contraseña inválida" })
+        return done(null, user)
+    }))
+
+    passport.use('github', new GithubStrategy({
+        clientID: config.github.CLIENT_ID,
+        clientSecret: config.github.SECRET,
+        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
+    }, async (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json;
+        const user = await userModel.findOne({email});
+        if (!user) {
+            const newUser = {
+                first_name: name,
+                email,
+                password: ''
+            }
+            const result = await userModel.create(newUser);
+            return done(null, result);
+        }
+        done(null, user);
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id)
+    })
+    passport.deserializeUser(async (id, done) => {
+        const result = await userModel.findOne({_id: id})
+        done(null, result);
+    })
+
+}
+
+export default initializeStrategies;
---
+++ desafio12/src/routes/views.router.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/register',(req,res)=>{
+    res.render('register');
+})
+
+router.get('/login', (req,res)=>{
+    if (req.session.user) {
+        res.redirect('/home')
+    } else {
+        res.render('login')
+    }
+})
+
+router.get('/home', async (req,res) => {
+    res.render('home', {user: req.session.user})
+})
+
+router.get('/logout', async (req, res) => {
+    res.render('logout', {user: req.session.user});
+    req.session.destroy()
+})
+
+export default router;
